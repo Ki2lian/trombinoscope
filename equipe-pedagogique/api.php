87ecbb27828b483e41929254f5f1607d610fa58d
Ki2lian <kignon12@gmail.com>
2020-05-04 20:41:19 +0200

    Ajout des cookies et fonctionnel.

--- equipe-pedagogique/api.php
+++ equipe-pedagogique/api.php
@@ -2,6 +2,14 @@
 
 $jsonTextApiFiliere = file_get_contents("https://etudiants.alwaysdata.net/filiere.json");
 $jsonArrayApiFiliere = json_decode($jsonTextApiFiliere, True);
+$key = "UikPqwDB8c1SHlFAn6FoMryc3610OMbZ";
+
+if (!empty($_POST["filiere"]) && !empty($_POST["groupe"])) {
+	$tabTrombi["filière"] = $_POST["filiere"];
+    $tabTrombi["groupe"] = $_POST["groupe"];
+    $jsonTabTrombi = json_encode($tabTrombi);
+  	setcookie("trombi", $jsonTabTrombi, time() +365*24*3600);
+}
 
 ?>
 <!DOCTYPE html>
@@ -56,75 +64,102 @@ function menu(){
 	echo "<input class=\"input-api\" type=\"button\" onclick=\"hideAllInfo();window.print();\" value=\"Imprimer\" />";
 	echo "</div>";
 }
+
+function filiereAndGroupe($filiere, $groupe, $key){
+	
+	$jsonTextApiTrombi = file_get_contents("https://etudiants.alwaysdata.net/test_api?groupe=$groupe&filiere=$filiere&key=$key");
+	$jsonArrayApiTrombi = json_decode($jsonTextApiTrombi, True);
+
+	if (isset($jsonArrayApiTrombi[$filiere][$groupe])) {
+		echo "<h2 class=\"h2-api\">Le groupe $groupe de la filière $filiere compte " . sizeof($jsonArrayApiTrombi[$filiere][$groupe]). " élèves</h2><br/>\n";
+		menu();
+		echo "<div class=\"container-api\">";
+		for ($i=1; $i <= sizeof($jsonArrayApiTrombi[$filiere][$groupe]); $i++) {
+			$info = $jsonArrayApiTrombi[$filiere][$groupe][$i];
+			echo "<div class=\"div-api\">\n";
+			echo "<img src=\"https://etudiants.alwaysdata.net/img/profil/" . $info["image"] . "\" onclick='showInfo(\"profil-" . $i ."\")' alt=\"Erreur\"/><br/>\n";
+			echo "<p>" . $info["nom"] . " " . $info["prenom"] . "</p>\n";
+			echo "<p class=\"profil-info\" id=\"profil-" . $i . "\" style=\"display: none;\">" . $info["email"] . "<br/>" . $info["telephone"] . "</p>\n";
+			echo "</div>\n";
+		}
+		echo "</div>";
+	}
+}
+
+function filiereOnly($filiere,$groupe,$key){
+	$jsonTextApiTrombi = file_get_contents("https://etudiants.alwaysdata.net/test_api?filiere=$filiere&key=$key");
+	$jsonArrayApiTrombi = json_decode($jsonTextApiTrombi, True);
+	
+	$jsonTextApiFiliere = file_get_contents("https://etudiants.alwaysdata.net/filiere.json");
+	$jsonArrayApiFiliere = json_decode($jsonTextApiFiliere, True);
+	for ($i=0; $i < sizeof($jsonArrayApiFiliere["filiere"]); $i++) { 
+		if ($filiere == $jsonArrayApiFiliere["filiere"][$i]["nom"]) {
+			break;
+		}
+	}
+
+	// Permet de récupérer le nom des groupes correspondant à la filière recherchée
+	$stockGroupe = array();
+	for ($k=0; $k < sizeof($jsonArrayApiFiliere["filiere"][$i]["groupe"]); $k++) { 
+		$stockGroupe[$k] = $jsonArrayApiFiliere["filiere"][$i]["groupe"][$k];
+	}
+
+	// Nombre d'élèves
+
+	for ($j=0; $j < sizeof($stockGroupe); $j++) { 
+		$nombreEleves += sizeof($jsonArrayApiTrombi[$filiere][$stockGroupe[$j]]);
+	}
+
+	echo "<h2 class=\"h2-api\">La filière $filiere compte " . $nombreEleves . " élèves</h2><br/>\n";
+	menu();
+
+
+	echo "<div class=\"container-api\">";
+	$stockAlea = array();
+	for ($j=0; $j < sizeof($stockGroupe); $j++) {
+		$groupe = $stockGroupe[$j];
+		for ($i=0; $i < sizeof($jsonArrayApiTrombi[$filiere][$groupe]); $i++) {
+			$info = $jsonArrayApiTrombi[$filiere][$groupe][$i];
+			echo "<div class=\"div-api\">\n";
+			$alea = mt_rand(0,99999); // on prend une valeur aléatoire car $i recommence à 0 donc va faire des doublons
+			while (in_array($alea, $stockAlea)) {
+				$alea = mt_rand(0,99999);
+			}
+			$stockAlea[] = $alea;
+			echo "<img src=\"https://etudiants.alwaysdata.net/img/profil/" . $info["image"] . "\" onclick='showInfo(\"profil-" . $alea ."\")' alt=\"Erreur\"/><br/>\n";
+			echo "<p>" . $info["nom"] . " " . $info["prenom"] . "</p>\n";
+			echo "<p class=\"profil-info\" id=\"profil-" . $alea . "\" style=\"display: none;\">" . $info["email"] . "<br/>" . $info["telephone"] . "</p>\n";
+			echo "</div>\n";
+		}
+	}
+	echo "</div>";
+}
+
+if (isset($_COOKIE["trombi"]) && !isset($_POST["form-trombinoscope"])) {
+	$jsonTabTrombi = $_COOKIE["trombi"];
+ 	$tabTrombi = json_decode($jsonTabTrombi,True);
+ 	$filiere = $tabTrombi["filière"];
+ 	$groupe = $tabTrombi["groupe"];
+
+ 	if ($groupe != "filiere-only") {
+ 		filiereAndGroupe($filiere,$groupe,$key);
+ 	}else{
+ 		filiereOnly($filiere,$groupe,$key);
+ 	}
+}
+
+
 if (isset($_POST["form-trombinoscope"])) {
 	if (!empty($_POST["filiere"]) && !empty($_POST["groupe"])) {
 		$filiere = $_POST["filiere"];
 		$groupe = $_POST["groupe"];
-		$key = "UikPqwDB8c1SHlFAn6FoMryc3610OMbZ";
-		
 
 		if ($groupe != "filiere-only") {
-			$jsonTextApiTrombi = file_get_contents("https://etudiants.alwaysdata.net/test_api?groupe=$groupe&filiere=$filiere&key=$key");
-			$jsonArrayApiTrombi = json_decode($jsonTextApiTrombi, True);
-			if (isset($jsonArrayApiTrombi[$filiere][$groupe])) {
-				echo "<h2 class=\"h2-api\">Le groupe $groupe de la filière $filiere compte " . sizeof($jsonArrayApiTrombi[$filiere][$groupe]). " élèves</h2><br/>\n";
-				menu();
-				echo "<div class=\"container-api\">";
-				for ($i=1; $i <= sizeof($jsonArrayApiTrombi[$filiere][$groupe]); $i++) {
-					$info = $jsonArrayApiTrombi[$filiere][$groupe][$i];
-					echo "<div class=\"div-api\">\n";
-					echo "<img src=\"https://etudiants.alwaysdata.net/img/profil/" . $info["image"] . "\" onclick='showInfo(\"profil-" . $i ."\")' alt=\"Erreur\"/><br/>\n";
-					echo "<p>" . $info["nom"] . " " . $info["prenom"] . "</p>\n";
-					echo "<p class=\"profil-info\" id=\"profil-" . $i . "\" style=\"display: none;\">" . $info["email"] . "<br/>" . $info["telephone"] . "</p>\n";
-					echo "</div>\n";
-				}
-				echo "</div>";
-			}
+			filiereAndGroupe($filiere,$groupe,$key);
 		}else{
-			$jsonTextApiTrombi = file_get_contents("https://etudiants.alwaysdata.net/test_api?filiere=$filiere&key=$key");
-			$jsonArrayApiTrombi = json_decode($jsonTextApiTrombi, True);
-			for ($i=0; $i < sizeof($jsonArrayApiFiliere["filiere"]); $i++) { 
-				if ($filiere == $jsonArrayApiFiliere["filiere"][$i]["nom"]) {
-					break;
-				}
-			}
-
-			// Permet de récupérer le nom des groupes correspondant à la filière recherchée
-			$stockGroupe = array();
-			for ($k=0; $k < sizeof($jsonArrayApiFiliere["filiere"][$i]["groupe"]); $k++) { 
-				$stockGroupe[$k] = $jsonArrayApiFiliere["filiere"][$i]["groupe"][$k];
-			}
-
-			// Nombre d'élèves
-
-			for ($j=0; $j < sizeof($stockGroupe); $j++) { 
-				$nombreEleves += sizeof($jsonArrayApiTrombi[$filiere][$stockGroupe[$j]]);
-			}
-
-			echo "<h2 class=\"h2-api\">La filière $filiere compte " . $nombreEleves . " élèves</h2><br/>\n";
-			menu();
-
-
-			echo "<div class=\"container-api\">";
-			$stockAlea = array();
-			for ($j=0; $j < sizeof($stockGroupe); $j++) {
-				$groupe = $stockGroupe[$j];
-				for ($i=0; $i < sizeof($jsonArrayApiTrombi[$filiere][$groupe]); $i++) {
-					$info = $jsonArrayApiTrombi[$filiere][$groupe][$i];
-					echo "<div class=\"div-api\">\n";
-					$alea = mt_rand(0,99999); // on prend une valeur aléatoire car $i recommence à 0 donc va faire des doublons
-					while (in_array($alea, $stockAlea)) {
-						$alea = mt_rand(0,99999);
-					}
-					$stockAlea[] = $alea;
-					echo "<img src=\"https://etudiants.alwaysdata.net/img/profil/" . $info["image"] . "\" onclick='showInfo(\"profil-" . $alea ."\")' alt=\"Erreur\"/><br/>\n";
-					echo "<p>" . $info["nom"] . " " . $info["prenom"] . "</p>\n";
-					echo "<p class=\"profil-info\" id=\"profil-" . $alea . "\" style=\"display: none;\">" . $info["email"] . "<br/>" . $info["telephone"] . "</p>\n";
-					echo "</div>\n";
-				}
-			}
-			echo "</div>";
+			filiereOnly($filiere,$groupe,$key);
 		}
+		
 	}else{
 		$erreur = "Tous les champs doivent être complétés.";}
 }
@@ -132,7 +167,7 @@ if (isset($_POST["form-trombinoscope"])) {
 
 
 <?php
-if (empty($_POST["filiere"])) {
+if (empty($_POST["filiere"]) && !isset($_COOKIE["trombi"])) {
 ?>
 	<div class="formulaire-trombi">
 		<form method="post">
